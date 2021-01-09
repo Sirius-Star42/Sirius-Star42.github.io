979e866a2fdd4c404a946cf0c7e5d46fcdb84707
Sirius-Star42 <67394008+Sirius-Star42@users.noreply.github.com>
2021-01-09 16:53:00 +0300

    passport package installed

--- Coursera/Assigment2-Node-Mongo/conFusionServer/app.js
+++ Coursera/Assigment2-Node-Mongo/conFusionServer/app.js
@@ -38,55 +38,32 @@ app.use(session({
   store: new FileStore() 
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    
-    if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-  
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var username = auth[0];
-    var password = auth[1];
-    
-    if (username === 'admin' && password == 'password') {
-      req.session.user = 'admin';
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
+    var err = new Error('You are not authenticated!');
+    err.status = 401;
+    return next(err);
   }
   else {
-    if (req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') {
       next();
     }
     else {
       var err = new Error('You are not authenticated!');
-      
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
 }
 
-
 app.use(auth);
 
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 app.use('/promotions', promoRouter);
 app.use('/promotions/:promoId', promoRouter)
